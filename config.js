4ac1fc16146c0b3dd2fe794951937cc25d534351
Ollie <114703429+7vnth@users.noreply.github.com>
2024-09-08 21:50:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
-    localhost: "",
+    localhost: "9987",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
